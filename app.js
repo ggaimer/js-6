ee3a1c668714e75f906aad99ab4f160f214bd221
gosha <45@.ru>
2023-10-06 22:44:16 +0500

    js-6

---
+++ app.js
@@ -0,0 +1,17 @@
+// const users = {};
+
+// for (let i = 1; i < Infinity; i++) {
+//     let command = prompt('Введите команду')
+//     let words = command.split(', ')
+//     if (words[0] == 'add') {
+//         users.push(words[1])
+//     }else if (words[0] == 'del') {
+//         for (let key in users) {
+//             users[key] == words[1] ? users.splice(key, 1) : ''
+//         }
+//     }else if (words[0] == 'stop') {
+//         break
+//     }
+// }
+
+// console.log(users);
